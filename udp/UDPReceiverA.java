18c740ff3afab8e9f09f7a6da273e42fd08130b1
etcqez <etcqez@outlook.com>
2024-11-28 21:23:20 +0800

    M udp/UDPReceiverA.java

--- udp/UDPReceiverA.java
+++ udp/UDPReceiverA.java
@@ -1,14 +1,16 @@
 package udp;
 
 import java.net.DatagramSocket;
+import java.net.SocketException;
 
 public class UDPReceiverA {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException {
         //创建一个 DatagramSocket 对象，准备在9999接收数据
         DatagramSocket socket = new DatagramSocket(9999);
         //创建一个DatagramPacket对象，准备接收数据
         //一个数据包最大64k
         byte[] buf = new byte[1024];
+        
 
 
 
