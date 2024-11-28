997b208f7c6ec8536b6282b185112112f92e8012
etcqez <etcqez@outlook.com>
2024-11-28 21:20:49 +0800

    M udp/UDPReceiverA.java

--- udp/UDPReceiverA.java
+++ udp/UDPReceiverA.java
@@ -6,6 +6,8 @@ public class UDPReceiverA {
     public static void main(String[] args) {
         //创建一个 DatagramSocket 对象，准备在9999接收数据
         DatagramSocket socket = new DatagramSocket(9999);
-        
+        //创建一个Data
+
+
     }
 }
