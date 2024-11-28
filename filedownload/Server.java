6ad40e2d51a575fa8115a803033d1718972a5f33
etcqez <etcqez@outlook.com>
2024-11-28 21:51:35 +0800

    M filedownload/Server.java

--- filedownload/Server.java
+++ filedownload/Server.java
@@ -13,7 +13,9 @@ public class Server {
         InputStream inputStream = accept.getInputStream();
         byte[] bytes = new byte[1024];
         int len = 0;
-        while ((len = inputStream.read(bytes) != -1))
+        while ((len = inputStream.read(bytes)) != -1) {
+            
+        }
 
     }
 }
