a3d6bb58526c921ca1c79a48647b8fc34f0fb1e8
etcqez <etcqez@outlook.com>
2024-11-28 22:24:48 +0800

    M filedownload/Client.java

--- filedownload/Client.java
+++ filedownload/Client.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println();
+        System.out.println("请输入下载的文件名");
+        
     }
 }
