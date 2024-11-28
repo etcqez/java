926f8fb82b3018775cbefd6e430a7e5e40c77e59
etcqez <etcqez@outlook.com>
2024-11-28 21:53:30 +0800

    M filedownload/Server.java

--- filedownload/Server.java
+++ filedownload/Server.java
@@ -16,7 +16,7 @@ public class Server {
         String downloadFileName = "";
         while ((len = inputStream.read(bytes)) != -1) {
             downloadFileName += new String(bytes, 0, len);
-            System.out.println();
+            System.out.println("客启端");
 
         }
 
