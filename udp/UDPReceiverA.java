1622bd2816ac55827c975ac8d1c3f28386c5309c
etcqez <etcqez@outlook.com>
2024-11-28 21:27:32 +0800

    M udp/UDPReceiverA.java

--- udp/UDPReceiverA.java
+++ udp/UDPReceiverA.java
@@ -1,11 +1,12 @@
 package udp;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
 public class UDPReceiverA {
-    public static void main(String[] args) throws SocketException {
+    public static void main(String[] args) throws IOException {
         //创建一个 DatagramSocket 对象，准备在9999接收数据
         DatagramSocket socket = new DatagramSocket(9999);
         //创建一个DatagramPacket对象，准备接收数据
