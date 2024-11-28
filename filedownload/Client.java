3ac3698878c501cbfab0bb16585a579733c28c1d
etcqez <etcqez@outlook.com>
2024-11-28 22:26:52 +0800

    M filedownload/Client.java

--- filedownload/Client.java
+++ filedownload/Client.java
@@ -1,16 +1,19 @@
 package filedownload;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入下载的文件名");
         String downloadFileName = scanner.next();
 
         Socket socket = new Socket(InetAddress.getLocalHost(), 9999);
+        OutputStream outputStream = socket.getOutputStream();
         
     }
 }
