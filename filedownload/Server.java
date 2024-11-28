31a172611f0a08c3281d4459ccdd7e924feb21a1
etcqez <etcqez@outlook.com>
2024-11-28 21:46:44 +0800

    M filedownload/Server.java

--- filedownload/Server.java
+++ filedownload/Server.java
@@ -1,10 +1,11 @@
 package filedownload;
 
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
         ServerSocket serverSocket = new ServerSocket(9999);
-        serverSocket.accept()
+        Socket accept = serverSocket.accept();
     }
 }
