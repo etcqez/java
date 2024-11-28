f5563d040263ea758b60ea8c7aceaf46b92b84b8
etcqez <etcqez@outlook.com>
2024-11-28 21:47:03 +0800

    M filedownload/Server.java

--- filedownload/Server.java
+++ filedownload/Server.java
@@ -1,11 +1,13 @@
 package filedownload;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(9999);
         Socket accept = serverSocket.accept();
+        
     }
 }
