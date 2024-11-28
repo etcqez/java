38af17c103e2f11a6abec42a7ccd35cf8d6d8b61
etcqez <etcqez@outlook.com>
2024-11-28 22:24:35 +0800

    M filedownload/Client.java

--- filedownload/Client.java
+++ filedownload/Client.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.println();
     }
 }
